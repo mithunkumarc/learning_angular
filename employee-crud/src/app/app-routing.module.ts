bd52d9e19a73daa3caede34a38bfc1c5ca5fe5f0
mithunkumarc <mithunchannappa@gmail.com>
2019-12-31 20:49:58 +0530

    Add files via upload

---
+++ employee-crud/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CrudComponent } from './crud/crud.component';
+
+
+
+const routes: Routes = [
+  { path: 'crud', component: CrudComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
